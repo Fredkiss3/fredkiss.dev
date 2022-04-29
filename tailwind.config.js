4468e874e361bb5d6db2856d1d376e27aab193f8
fredhel KISSIE <fredkiss3gmail.com>
2022-04-29 23:58:36 +0200

    🍱  Setup Tailwind CSS for styles

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#F2643E",
+        secondary: "#35766A",
+        tertiary: "#FFCD60",
+        dark: "#264754",
+        light: "#F8F7F1",
+      },
+
+      fontFamily: {
+        custom: ["Square Peg", "cursive"],
+      },
+    },
+  },
+  plugins: [],
+};
