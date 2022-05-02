258eb4b5cbff11ce1748f0a6e472fa603f6b9bdf
fredhel KISSIE <fredkiss3@gmail.com>
2022-05-03 00:48:47 +0200

    💄 mobile menu finished

---
+++ components/header.tsx
@@ -0,0 +1,99 @@
+import React from "react";
+import NextLink from "next/link";
+import { clsx } from "../lib/webutils";
+import { MenuIcon } from "@heroicons/react/solid";
+import { Link } from "./link";
+import { motion } from "framer-motion";
+
+export function Header({}) {
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+  const containerAnimationOptions = {
+    hide: { opacity: 1 },
+    show: {
+      opacity: 1,
+      transition: {
+        staggerChildren: 0.1,
+      },
+    },
+  };
+
+  const itemAnimationOptions = {
+    hide: { opacity: 0 },
+    show: { opacity: 1 },
+  };
+
+  return (
+    <>
+      <header className={`bg-light px-4 pt-4 fixed top-0 left-0 right-0 z-10`}>
+        <div className="flex justify-between items-center">
+          <NextLink href={`/`}>
+            <a className="font-custom text-4xl font-bold">Adrien</a>
+          </NextLink>
+
+          <button
+            className="rounded-full bg-white p-2 shadow-xl"
+            onClick={() => setIsMenuOpen(!isMenuOpen)}
+          >
+            <MenuIcon className="h-5" />
+          </button>
+        </div>
+
+        {isMenuOpen && (
+          <nav
+            className={clsx(
+              `
+              fixed 
+              top-[4.3rem]
+              bottom-0 left-0 right-0 
+              text-left
+            bg-light z-20`,
+              "bg-opacity-75 backdrop-blur-md",
+              "md:hidden",
+              "flex flex-col items-center justify-center"
+            )}
+          >
+            <motion.ul
+              className="flex flex-col gap-4"
+              key={"header"}
+              variants={containerAnimationOptions}
+              initial="hide"
+              animate="show"
+            >
+              <motion.li
+                key={`projects`}
+                variants={itemAnimationOptions}
+                transition={{
+                  duration: 0.1,
+                }}
+              >
+                <Link href="#projects">Projets</Link>
+              </motion.li>
+
+              <motion.li
+                key={`experience`}
+                variants={itemAnimationOptions}
+                transition={{
+                  duration: 0.1,
+                }}
+              >
+                <Link href="#experience">Expérience</Link>
+              </motion.li>
+
+              <motion.li
+                key={`about`}
+                variants={itemAnimationOptions}
+                transition={{
+                  duration: 0.1,
+                }}
+              >
+                <Link href="#about">A propos de moi</Link>
+              </motion.li>
+            </motion.ul>
+          </nav>
+        )}
+
+        <hr className="mt-3 border-gray-200" />
+      </header>
+    </>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,15 @@
 import type { GetStaticProps, NextPage } from "next";
 import Head from "next/head";
 
+import * as React from "react";
+
 // import { MDXRemote } from "next-mdx-remote";
 import { getExperiences } from "../lib/functions";
 import { Experience, MarkdownData } from "../lib/types";
-import { MenuIcon } from "@heroicons/react/solid";
-import Link from "next/link";
 import Image from "next/image";
+
+import { Header } from "../components/header";
+import { Footer } from "../components/footer";
 
 type HomePageProps = {
   experiences: MarkdownData<Experience>[];
@@ -22,82 +25,93 @@ const Home: NextPage<HomePageProps> = (props) => {
         <link rel="icon" href="/favicon.png" />
       </Head>
 
-      <header className={`bg-light px-4 pt-4 fixed top-0 left-0 right-0`}>
-        <div className="flex justify-between items-center">
-          <Link href={`/`}>
-            <a className="font-custom text-4xl font-bold">Adrien</a>
-          </Link>
-
-          <button className="rounded-full bg-white p-2 shadow-xl">
-            <MenuIcon className="h-5" />
-          </button>
-        </div>
-
-        <hr className="mt-3 border-gray-200" />
-      </header>
+      <Header />
 
       <main className="bg-light px-4 mt-[4.2rem] pt-10">
-        <section className="flex flex-col gap-4">
-          <h1 className="text-4xl font-bold leading-relaxed">
-            Hello wold, je suis Adrien KISSIE
-          </h1>
-
-          <p>
-            Développeur web et streamer sur&nbsp;
-            <a
-              href="https://twitch.tv/fredkisss"
-              target={`_blank`}
-              className="text-primary underline"
-            >
-              twitch
-            </a>
-            , je crée des applications web orientées performance du{" "}
-            <strong className="font-bold">backend</strong> au{" "}
-            <strong className="font-bold">frontend</strong>.
-          </p>
-
-          <div className="relative h-96">
-            <div className="absolute top-10 left-0 right-0">
-              <img
-                src="/brush-stroke-1.png"
-                alt="brush stroke"
-                className="rotate-12"
-                style={{
-                  filter:
-                    "invert(24%) sepia(56%) saturate(3292%) hue-rotate(161deg) brightness(86%) contrast(102%)",
-                }}
-              />
-              <img
-                src="/brush-stroke-2.png"
-                alt="brush stroke"
-                className="-rotate-[20deg]"
-                style={{
-                  filter:
-                    "invert(24%) sepia(56%) saturate(3292%) hue-rotate(161deg) brightness(86%) contrast(102%)",
-                }}
-              />
-              <img
-                src="/brush-stroke-3.png"
-                alt="brush stroke"
-                className="-rotate-[25deg]"
-                style={{
-                  filter:
-                    "invert(24%) sepia(56%) saturate(3292%) hue-rotate(161deg) brightness(86%) contrast(102%)",
-                }}
-              />
-            </div>
-            <Image
-              src="/adrien.png"
-              layout="fill"
-              objectFit="cover"
-              alt="Photo de Adrien KISSIE"
-            />
-          </div>
-        </section>
+        <StartingSection />
+        <ProjectSection />
+        <ExperienceSection />
+        <AboutSection />
       </main>
+
+      <Footer />
     </div>
   );
 };
+
+function StartingSection() {
+  return (
+    <section className="flex flex-col gap-4">
+      <h1 className="text-4xl font-bold leading-relaxed">
+        Hello world, je suis Adrien KISSIE
+      </h1>
+
+      <p>
+        Développeur web et streamer sur&nbsp;
+        <a
+          href="https://twitch.tv/fredkisss"
+          target={`_blank`}
+          className="text-primary underline"
+        >
+          twitch
+        </a>
+        , je crée des applications web orientées performance du{" "}
+        <strong className="font-bold">backend</strong> au{" "}
+        <strong className="font-bold">frontend</strong>.
+      </p>
+
+      <div className="relative h-96 overflow-hidden">
+        <div className="absolute top-10 left-0 right-0">
+          <img
+            src="/brush-stroke-1.png"
+            alt="brush stroke"
+            className="rotate-12"
+            style={{
+              filter:
+                "invert(24%) sepia(56%) saturate(3292%) hue-rotate(161deg) brightness(86%) contrast(102%)",
+            }}
+          />
+          <img
+            src="/brush-stroke-2.png"
+            alt="brush stroke"
+            className="-rotate-[20deg]"
+            style={{
+              filter:
+                "invert(24%) sepia(56%) saturate(3292%) hue-rotate(161deg) brightness(86%) contrast(102%)",
+            }}
+          />
+          <img
+            src="/brush-stroke-3.png"
+            alt="brush stroke"
+            className="-rotate-[25deg]"
+            style={{
+              filter:
+                "invert(24%) sepia(56%) saturate(3292%) hue-rotate(161deg) brightness(86%) contrast(102%)",
+            }}
+          />
+        </div>
+        <Image
+          src="/adrien.png"
+          layout="fill"
+          objectFit="cover"
+          alt="Photo de Adrien KISSIE"
+        />
+      </div>
+    </section>
+  );
+}
+
+function ProjectSection() {
+  return <section></section>;
+}
+
+function ExperienceSection() {
+  return <section></section>;
+}
+
+function AboutSection() {
+  return <section></section>;
+}
 
 export default Home;
 
