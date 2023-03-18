18c8d90a6a52b99b76805443dbf8c0023600432c
Adrien KISSIE <fredkiss3@gmail.com>
2023-03-18 16:01:03 +0100

    🔥 removed unused types

--- lib/types.ts
+++ lib/types.ts
@@ -1,41 +1,4 @@
 import { Route } from "next";
-import type { MDXRemoteSerializeResult } from "next-mdx-remote";
-
-export type Experience = {
-  company: string;
-  type: string;
-  title: string;
-  startDate: string;
-  endDate?: string;
-  color: "primary" | "secondary" | "tertiary";
-  technologies: string[];
-};
-
-export type Project = {
-  name: string;
-  image: string;
-  startDate: string;
-  color: "secondary" | "tertiary" | "dark" | "light";
-  technologies: string[];
-  url?: string;
-  github?: string;
-};
-
-export type Skill = {
-  id: string;
-  name: string;
-  technologies: string[];
-};
-
-export type MarkdownData<
-  T extends {
-    [key: string]: any;
-  }
-> = {
-  data: T;
-  content: string;
-  serializedContent: MDXRemoteSerializeResult;
-};
 
 export type NavLink = {
   href: Route;
