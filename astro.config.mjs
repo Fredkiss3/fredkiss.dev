1e562b07ea8b907a14fed91cf15613b4b30fd56e
Adrien KISSIE <fredkiss3@gmail.com>
2023-12-31 19:49:13 +0100

    🚧 i18n + vercel integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,19 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import markdoc from "@astrojs/markdoc";
 import keystatic from "@keystatic/astro";
-
 import tailwind from "@astrojs/tailwind";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en", "fr"],
+    routing: {
+      prefixDefaultLocale: false
+    }
+  },
   integrations: [
     react(),
     markdoc(),
@@ -15,5 +23,10 @@ export default defineConfig({
       applyBaseStyles: false
     })
   ],
-  output: "hybrid"
+  output: "hybrid",
+  adapter: vercel({
+    speedInsights: {
+      enabled: true
+    }
+  })
 });
