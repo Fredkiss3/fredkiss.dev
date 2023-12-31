c3997d82e3076860f9fb47903c25778329147111
Adrien KISSIE <fredkiss3@gmail.com>
2023-12-31 18:52:25 +0100

    🔧 setup tailwind + custom fonts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,19 @@
 import { defineConfig } from "astro/config";
-
 import react from "@astrojs/react";
 import markdoc from "@astrojs/markdoc";
 import keystatic from "@keystatic/astro";
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [react(), markdoc(), keystatic()],
-	output: "hybrid",
+  integrations: [
+    react(),
+    markdoc(),
+    keystatic(),
+    tailwind({
+      applyBaseStyles: false
+    })
+  ],
+  output: "hybrid"
 });
