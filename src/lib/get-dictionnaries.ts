a02a673d4984d0960aaac75554cf4375dbe9fcf5
Adrien KISSIE <fredkiss3@gmail.com>
2024-01-02 00:38:19 +0100

    ♻️ move i18n utils into one file

--- src/lib/get-dictionnaries.ts
+++ src/lib/get-dictionnaries.ts
@@ -1,11 +1,10 @@
-import type { Locale } from "~/lib/i18n-config";
+import type { Locale } from "~/lib/i18n-utils";
 
 const dictionaries = {
   en: () => import("~/dictionaries/en.json").then((module) => module.default),
   fr: () => import("~/dictionaries/fr.json").then((module) => module.default)
-};
+} satisfies Record<Locale, unknown>;
 
 export const getDictionary = async (locale: Locale) => dictionaries[locale]();
 
 export type TranslationDictionary = Awaited<ReturnType<typeof getDictionary>>;
-export type Lang = keyof typeof dictionaries;
