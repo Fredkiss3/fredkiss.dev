6f19ac92fef7dc59c1bf71b84d797f5ac15a3f13
Adrien KISSIE <fredkiss3@gmail.com>
2024-09-23 01:52:33 +0200

    🚧 wip

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,5 +1,18 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import path from "node:path";
+import { defineConfig } from "astro/config";
+
+import tailwind from "@astrojs/tailwind";
+const __dirname = import.meta.dirname;
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	integrations: [tailwind()],
+	vite: {
+		resolve: {
+			alias: {
+				"~": path.resolve(__dirname, "./src"),
+			},
+		},
+	},
+});
