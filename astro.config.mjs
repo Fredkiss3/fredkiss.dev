91bd948db574ece98021994638ba9ea0ffdf7449
Fred KISSIE <fredkiss3@gmail.com>
2025-03-02 21:33:01 +0100

    🎨 format

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,26 @@
 // @ts-check
 import path from "node:path";
-import { defineConfig } from "astro/config";
 import node from "@astrojs/node";
 import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "astro/config";
 
 import sitemap from "@astrojs/sitemap";
 const __dirname = import.meta.dirname;
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://fredkiss.dev",
-	integrations: [sitemap()],
-	vite: {
-		plugins: [tailwindcss()],
-		resolve: {
-			alias: {
-				"~": path.resolve(__dirname, "./src"),
-			},
-		},
-	},
-	output: "static",
-	adapter: node({
-		mode: "standalone",
-	}),
+  site: "https://fredkiss.dev",
+  integrations: [sitemap()],
+  vite: {
+    plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        "~": path.resolve(__dirname, "./src")
+      }
+    }
+  },
+  output: "static",
+  adapter: node({
+    mode: "standalone"
+  })
 });
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	darkMode: "media",
-	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  darkMode: "media",
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {}
+  },
+  plugins: []
 };
