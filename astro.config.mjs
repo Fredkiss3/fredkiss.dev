37f08c9953f7a21ae6388a23847b9acc7ca5bc88
Fred KISSIE <fredkiss3@gmail.com>
2025-03-02 19:30:45 +0100

    ♻️ refactor

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,17 @@
 import path from "node:path";
 import { defineConfig } from "astro/config";
 import node from "@astrojs/node";
+import tailwindcss from "@tailwindcss/vite";
 
-import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 const __dirname = import.meta.dirname;
 
 // https://astro.build/config
 export default defineConfig({
 	site: "https://fredkiss.dev",
-	integrations: [tailwind(), sitemap()],
+	integrations: [sitemap()],
 	vite: {
+		plugins: [tailwindcss()],
 		resolve: {
 			alias: {
 				"~": path.resolve(__dirname, "./src"),
