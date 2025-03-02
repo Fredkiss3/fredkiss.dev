9421a202a119aa4732e19de7b107efcf02842e2a
Fred KISSIE <fredkiss3@gmail.com>
2025-03-02 22:54:37 +0100

    ♻️ set cache-header to public

--- src/pages/api/[project]/stars.json.ts
+++ src/pages/api/[project]/stars.json.ts
@@ -46,8 +46,8 @@ export const GET: APIRoute = async function get({ params }) {
   );
 
   const cacheHeader = import.meta.env.PROD
-    ? "s-max-age=1800, stale-while-revalidate=1800, stale-if-error=43200"
-    : "stale-while-revalidate=1800, stale-if-error=43200";
+    ? "public, s-max-age=1800, stale-while-revalidate=1800, stale-if-error=43200"
+    : "private, no-cache";
 
   return Response.json(
     {
