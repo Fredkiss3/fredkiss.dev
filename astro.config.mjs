e1166fcd23fb9b62258f44f35a0ad8664a6a9a5c
Fred KISSIE <fredkiss3@gmail.com>
2025-07-28 04:27:17 +0200

    🌐 add i18n

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import path from "node:path";
 import node from "@astrojs/node";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
+import { defaultLocale, locales } from "./src/i18n/config.mjs";
 
 import sitemap from "@astrojs/sitemap";
 const __dirname = import.meta.dirname;
@@ -10,6 +11,15 @@ const __dirname = import.meta.dirname;
 // https://astro.build/config
 export default defineConfig({
   site: "https://fredkiss.dev",
+  i18n: {
+    locales,
+    defaultLocale,
+    routing: {
+      prefixDefaultLocale: true,
+      redirectToDefaultLocale: false,
+      fallbackType: "redirect"
+    }
+  },
   integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()],
